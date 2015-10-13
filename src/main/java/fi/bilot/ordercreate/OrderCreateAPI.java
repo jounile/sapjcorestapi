42d2b45a74f707e85e5939b60a6fccab302c812f
jounile <jouni.leino@bilot.fi>
2015-10-13 15:16:56 +0300

    Modifying ordercreate API

--- src/main/java/fi/bilot/ordercreate/OrderCreateAPI.java
+++ src/main/java/fi/bilot/ordercreate/OrderCreateAPI.java
@@ -24,37 +24,62 @@ public class OrderCreateAPI {
 
 			// Import
 			
+			JCoParameterList importParams = function.getImportParameterList();
+			importParams.setValue("SALESDOCUMENT", "");
+			importParams.setValue("SENDER", "");
+			importParams.setValue("BINARY_RELATIONSHIPTYPE", "");
+			importParams.setValue("INT_NUMBER_ASSIGNMENT", "");
+			importParams.setValue("BEHAVE_WHEN_ERROR", "P");
+			importParams.setValue("LOGIC_SWITCH", "");
+			importParams.setValue("BUSINESS_OBJECT", "");
+			importParams.setValue("TESTRUN", "X"); // X to simulate order
+			importParams.setValue("CONVERT_PARVW_AUART", "");
+			importParams.setValue("STATUS_BUFFER_REFRESH", "X");
+			
 			JCoStructure sales_header_in = function.getImportParameterList().getStructure("SALES_HEADER_IN");
 			sales_header_in.setValue("DOC_TYPE", "ZOR");
 			sales_header_in.setValue("SALES_ORG", "0001");
 			sales_header_in.setValue("DISTR_CHAN", "01");
 			sales_header_in.setValue("DIVISION", "01");
+			sales_header_in.setValue("REQ_DATE_H", new Date(115, 10, 12));
+			sales_header_in.setValue("PURCH_NO_C", "123456789");
 			//sales_header_in.setValue("DUN_COUNT", "0");
 			//sales_header_in.setValue("ADD_VAL_DY", "00");
 			//sales_header_in.setValue("EXCHG_RATE", "0,00000");
-			sales_header_in.setValue("REQ_DATE_H", new Date(115, 10, 12));
 			//sales_header_in.setValue("CT_VALID_T", "");
 			//sales_header_in.setValue("PO_METHOD", "");
 			//sales_header_in.setValue("DLV_BLOCK", "");
 			//sales_header_in.setValue("REF_DOC", "");
 			//sales_header_in.setValue("REFDOC_CAT", "B");
-			sales_header_in.setValue("PURCH_NO_C", "123456789");
 			
-			JCoStructure sales_header_inx = function.getImportParameterList().getStructure("SALES_HEADER_INX");			
-			//sales_header_inx.setValue("ITM_NUMBER", "000010");
-			//sales_header_inx.setValue("UPDATEFLAG", "I");
-			//sales_header_inx.setValue("MATERIAL", "X");
-			//sales_header_inx.setValue("TARGET_QTY", "X");
+			JCoStructure sales_header_inx = function.getImportParameterList().getStructure("SALES_HEADER_INX");
+			sales_header_inx.setValue("UPDATEFLAG", "I");
+			sales_header_inx.setValue("DOC_TYPE", "X");
+			sales_header_inx.setValue("SALES_ORG", "X");
+			sales_header_inx.setValue("DISTR_CHAN", "X");
+			sales_header_inx.setValue("DIVISION", "X");
+			sales_header_inx.setValue("REQ_DATE_H", "X");
+			sales_header_inx.setValue("PURCH_NO_C", "X");
+			
 			
 			JCoTable sales_items_in = function.getTableParameterList().getTable("SALES_ITEMS_IN");
 			sales_items_in.appendRow();
 			sales_items_in.setValue("ITM_NUMBER", "000010");
-			sales_items_in.setValue("MATERIAL", "A10000"); // ARLA RASVATON MAITO 1L
+			sales_items_in.setValue("MATERIAL", "A10000");
 			sales_items_in.setValue("TARGET_QTY", "1.000");
+			sales_items_in.setValue("SALES_UNIT", "ST");
+			//sales_items_in.setValue("PLANT", "0001");
 			//sales_items_in.setValue("TARGET_QTY", "00000001");
 			//sales_items_in.setValue("TARGET_QU", "ST");
-			sales_items_in.setValue("SALES_UNIT", "ST");
-							
+			
+			JCoTable sales_items_inx = function.getTableParameterList().getTable("SALES_ITEMS_INX");
+			sales_items_inx.appendRow();
+			sales_items_inx.setValue("ITM_NUMBER", "000010");
+			sales_items_inx.setValue("MATERIAL", "X");
+			sales_items_inx.setValue("TARGET_QTY", "X");
+			sales_items_inx.setValue("SALES_UNIT", "X");
+			//sales_items_inx.setValue("PLANT", "X");
+			
 			JCoTable sales_schedules_in = function.getTableParameterList().getTable("SALES_SCHEDULES_IN");
 			sales_schedules_in.appendRow();
 			sales_schedules_in.setValue("ITM_NUMBER", "000010");
@@ -68,19 +93,7 @@ public class OrderCreateAPI {
 			sales_partners.setValue("PARTN_NUMB", "0000000031");
 			
 			JCoTable sales_ccard = function.getTableParameterList().getTable("SALES_CCARD");
-			
-			JCoParameterList importParams = function.getImportParameterList();
-			importParams.setValue("SALESDOCUMENT", "");
-			importParams.setValue("SENDER", "");
-			importParams.setValue("BINARY_RELATIONSHIPTYPE", "");
-			importParams.setValue("INT_NUMBER_ASSIGNMENT", "");
-			importParams.setValue("BEHAVE_WHEN_ERROR", "P");
-			importParams.setValue("LOGIC_SWITCH", "");
-			importParams.setValue("BUSINESS_OBJECT", "");
-			importParams.setValue("TESTRUN", ""); // X to simulate order
-			importParams.setValue("CONVERT_PARVW_AUART", "");
-			importParams.setValue("STATUS_BUFFER_REFRESH", "X");
-			
+				
 
 			System.out.println("Calling SD_SALESDOCUMENT_CREATE");
 			function.execute(jcoDestination);
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,19 +1,36 @@
 <html>
 <body>
 <h1>SAP JCO REST API</h1>
-<h2>Available services</h2>
+<p>Available services</p>
+
+<h3>Order</h3>
+
 <ul>
 <li><a href="http://localhost:8080/sapjcorestapi/rest/order/5">/rest/order/[nro]</a></li>
+<li><a href="http://localhost:8080/sapjcorestapi/rest/ordercreate">/rest/ordercreate</a></li>
+</ul>
+
+<h3>User</h3>
+<ul>
 <li><a href="http://localhost:8080/sapjcorestapi/rest/user/DOEJAMES1">/rest/user/[username]</a></li>
+</ul>
+
+<h3>Customer</h3>
+<ul>
 <li><a href="http://localhost:8080/sapjcorestapi/rest/customer/1">/rest/customer</a> POST {"customerNro":"17","salesOrg":"0001","distributionChannel":"01","division":"01"}</li>
 <li><a href="http://localhost:8080/sapjcorestapi/rest/customersearch">/rest/customersearch</a> POST {"customerNro":"186*","salesOrg":"0001","distributionChannel":"01","division":"01"}</li>
+</ul>
+
+<h3>Flight</h3>
+<ul>
+<li><a href="http://localhost:8080/sapjcorestapi/rest/flight">/rest/flight</a> POST {"carrier":"LH","connectionNumber":"0400","date":"20151010"} </li>
+<li><a href="http://localhost:8080/sapjcorestapi/rest/flightlist">/rest/flightlist</a> POST {"fromCountry":"DE","fromCity":"FRANKFURT","toCountry":"US","toCity":"NEW YORK","maxRead":"12"} </li>
+</ul>
+
+
 
 <!--<li><a href="http://localhost:8080/sapjcorestapi/rest/orders/123">/rest/orders/nro</a></li>-->
 <!--<li><a href="http://localhost:8080/sapjcorestapi/rest/users/234/">/rest/users/customer</a></li>-->
 <!--<li><a href="http://localhost:8080/sapjcorestapi/rest/atp/222/">/rest/atp/material</a></li>-->
-
-<li><a href="http://localhost:8080/sapjcorestapi/rest/flight">/rest/flight</a> POST {"carrier":"LH","connectionNumber":"0400","date":"20151010"} </li>
-<li><a href="http://localhost:8080/sapjcorestapi/rest/flightlist">/rest/flightlist</a> POST {"fromCountry":"DE","fromCity":"FRANKFURT","toCountry":"US","toCity":"NEW YORK","maxRead":"12"} </li>
-</ul>
 </body>
 </html>
